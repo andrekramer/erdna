55131e10544164f2d50eee67e6d92c28211051d2
Andre Kramer <andre.equus@gmail.com>
2024-11-25 12:24:19 +0000

    gemini solves a logical problem

--- ai/gemini.scm
+++ ai/gemini.scm
@@ -7,6 +7,10 @@
 (define (ask-gemini query)
   (resolve (fetch-promise gemini-url "" query content-type)) )
 
-(define query "{\"contents\":[{\"parts\":[{\"text\":\"Explain how AI works\"}]}]}")
+;; (define text "Explain how AI works")
+
+(define text "There are three contries in a continent named Pandoma. Esland is next to MiddLand and MiddLand is next to Wesland. Esland and Wesland have no other neighbours than Middland. Middland has two neighbours which are Esland and Wesland. Esland is a Democracy while Wesland is a Communist country. Middland is either a Democracy or a Communist country. Is there a Democracy next to a Communist country in Pandoma?")
+
+(define query (concat "{\"contents\":[{\"parts\":[{\"text\":\"" text "\"}]}]}"))
 
 (ask-gemini query)
